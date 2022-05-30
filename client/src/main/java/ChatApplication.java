69229748b61c1c682f447fb45748058c73b99155
waterfuried <cisternsystem@gmail.com>
2022-05-31 01:28:35 +0300

    NetworkStorage repository

---
+++ client/src/main/java/ChatApplication.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ChatApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Chat");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
